0f93f4c22ef2b5f9cef4ca3216921fa9c886afe3
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-03-04 22:21:09 -0500

    finished assignment 3

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
-    res.json(user)
+    let { password, ...rest } = req.body;
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcrypt.hash(password, 10)
+    })
 })
 
 
